f1ee5b2284c0c0141ea3dc7aa65cac4f15f868e1
melanie197 <74988083+melanie197@users.noreply.github.com>
2021-02-05 17:18:17 -0500

    Add files via upload

---
+++ 08_json-parse3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 29 07:14:00 2021
+
+@author: pc
+"""
+#ENTRADAS DADAS POR EL USUARIO 
+import  urllib.parse
+import requests
+main_api="https://www.mapquestapi.com/directions/v2/route?"
+key="VIrvtp6n6tJt14jb5T6GXvZZpalQgR7f"
+
+while True: 
+    orig=input('ubicacion inicial: ')
+    dest=input('destino: ')
+    url= main_api+ urllib.parse.urlencode({"key": key,"from":orig,"to":dest})
+    print("URL: "+(url)) 
+    
+    json_data=requests.get(url).json()
+    json_status=json_data["info"]["statuscode"]
+    
+    if json_status==0:
+        print("API Status: " + str(json_status)+ " = A successfull route call.\n")
+   
