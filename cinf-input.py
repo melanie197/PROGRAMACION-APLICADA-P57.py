5b9923aeb5fc6c88fba2504ddc2361a63b914361
melanie197 <74988083+melanie197@users.noreply.github.com>
2021-02-08 17:18:21 -0500

    Add files via upload

---
+++ cinf-input.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 17:14:57 2021
+
+@author: pc
+"""
+
+nombre=input('Ingrese su nombre: ')
+apellido=input('Ingrese su apellido: ')
+edad=input('ingrese su edad: ')
+lugar=input('ingrese el lugar donde vive: ')
+space=" "
+print( )
+print('bienvenid@'+space+nombre+space+apellido+
+      'su edad es de '+space+edad+' se encuentra en '+space+lugar,space+
+      'gracias por toda su informacion '
+      'MUCHAS GRACIAS :)')
